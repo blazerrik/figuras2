2eb4acd493ab68d61058ef5499b24031e4c88747
Luis Enrique Garcia Murillo <garcia.luis.enrique19@gmail.com>
2016-10-03 11:11:47 -0500

    Agrega configuracion para CI Travis
    se agrego el archivo .travis.yml, requeriments.txt para poder realizar CO en travis.-ci.org

--- figuras.py
+++ figuras.py
@@ -1,37 +1,36 @@
 class Figuras:
+
     def cuadrado(self, lado):
         try:
             lado = float(lado)
             return lado * lado
-        except Exception as e:
+        except Exception:
             return 'dato incorrecto'
 
-
-    def rectangulo(self,base,altura):
+    def rectangulo(self, base, altura):
         try:
-            base=float(base)
-            altura=float(altura)
+            base = float(base)
+            altura = float(altura)
             return base * altura
-        except Exception as e:
+        except Exception:
             return 'dato incorrecto'
 
-    def triangulo(self,base, altura):
+    def triangulo(self, base, altura):
         try:
-            base=float(base)
-            altura=float(altura)
-            resultado = base *altura
-            return resultado/2
-        except Exception as e:
+            base = float(base)
+            altura = float(altura)
+            resultado = base * altura
+            return resultado / 2
+        except Exception:
             return 'dato incorrecto'
 
-
-    def circulo(self,radio):
+    def circulo(self, radio):
         try:
-            radio=float(radio)
+            radio = float(radio)
             pi = 3.1416
             radio = radio * radio
 
             resultado = pi * radio
             return resultado
-        except Exception as e:
+        except Exception:
             return 'dato incorrecto'
--- figurastest.py
+++ figurastest.py
@@ -1,5 +1,7 @@
 import unittest
 from figuras import Figuras
+
+
 class TestFiguras(unittest.TestCase):
 
     def setUp(self):
@@ -8,67 +10,67 @@ class TestFiguras(unittest.TestCase):
     def test_area_cuadrado_lado_5(self):
 
         resultado = self.figuras.cuadrado(5)
-        self.assertEqual(25,resultado)
+        self.assertEqual(25, resultado)
 
     def test_area_cuadrado_lado_6(self):
 
         resultado = self.figuras.cuadrado(6)
-        self.assertEqual(36,resultado)
+        self.assertEqual(36, resultado)
+
+    def test_area_cuadrado_lado_7(self):
+
+        resultado = self.figuras.cuadrado(7)
+        self.assertEqual(41, resultado)
 
     def test_area_cuadrado_lado_g(self):
 
         resultado = self.figuras.cuadrado('g')
-        self.assertEqual('dato incorrecto',resultado)
+        self.assertEqual('dato incorrecto', resultado)
 
     def test_area_cuadrado_lado_4_punto_5(self):
 
         resultado = self.figuras.cuadrado(4.5)
-        self.assertEqual(20.25,resultado)
-
+        self.assertEqual(20.25, resultado)
 
     def test_area_rectangulo_base_5_altura_4(self):
 
-        resultado = self.figuras.rectangulo(5,4)
-        self.assertEqual(20,resultado)
+        resultado = self.figuras.rectangulo(5, 4)
+        self.assertEqual(20, resultado)
 
     def test_area_rectangulo_base_7_altura_3(self):
 
-        resultado = self.figuras.rectangulo(7,3)
-        self.assertEqual(21,resultado)
-
+        resultado = self.figuras.rectangulo(7, 3)
+        self.assertEqual(21, resultado)
 
     def test_area_rectangulo_base_5_3_altura_4_2(self):
 
-        resultado = self.figuras.rectangulo(5.3,4.2)
-        self.assertEqual(22.26,resultado)
-
-
+        resultado = self.figuras.rectangulo(5.3, 4.2)
+        self.assertEqual(22.26, resultado)
 
     def test_area_rectangulo_base_gg(self):
 
-        resultado = self.figuras.rectangulo('gg','izi')
-        self.assertEqual('dato incorrecto',resultado)
+        resultado = self.figuras.rectangulo('gg', 'izi')
+        self.assertEqual('dato incorrecto', resultado)
 
     def test_area_triangulo_base_5_altura_4(self):
 
-        resultado = self.figuras.triangulo(5,4)
-        self.assertEqual(10,resultado)
+        resultado = self.figuras.triangulo(5, 4)
+        self.assertEqual(10, resultado)
 
     def test_area_triangulo_base_izi_gg(self):
 
         resultado = self.figuras.triangulo('izi', 'gg')
-        self.assertEqual('dato incorrecto',resultado)
-
+        self.assertEqual('dato incorrecto', resultado)
 
     def test_area_circulo_base_8(self):
 
         resultado = self.figuras.circulo(8)
-        self.assertEqual(201.0624,resultado)
+        self.assertEqual(201.0624, resultado)
 
     def test_area_circulo_base_izi_gg_circulo(self):
 
         resultado = self.figuras.circulo('circulo')
-        self.assertEqual('dato incorrecto',resultado)
+        self.assertEqual('dato incorrecto', resultado)
 
 if __name__ == '__main__':
     unittest.main()
